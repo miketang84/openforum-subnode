959f29359745b78d04a87e68eb9c68c072c53db7
MikeTang84 <daogangtang@gmail.com>
2022-08-06 18:51:57 +0800

    first commit.
    Signed-off-by: MikeTang84 <daogangtang@gmail.com>

---
+++ pallets/template/src/lib.rs
@@ -0,0 +1,95 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+pub use pallet::*;
+
+#[cfg(test)]
+mod mock;
+
+#[cfg(test)]
+mod tests;
+
+#[cfg(feature = "runtime-benchmarks")]
+mod benchmarking;
+
+type IdType = Vec<u8>;
+type HashType = Vec<u8>;
+
+#[frame_support::pallet]
+pub mod pallet {
+	use frame_support::pallet_prelude::*;
+	use frame_system::pallet_prelude::*;
+
+	#[pallet::pallet]
+	#[pallet::generate_store(pub(super) trait Store)]
+	pub struct Pallet<T>(_);
+
+	/// Configure the pallet by specifying the parameters and types on which it depends.
+	#[pallet::config]
+	pub trait Config: frame_system::Config {
+		/// Because this pallet emits events, it depends on the runtime's definition of an event.
+		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+	}
+
+	//#[pallet::storage]
+	//#[pallet::getter(fn something)]
+	//pub type Something<T> = StorageValue<_, u32>;
+
+    #[pallet::storage]
+    pub(super) type IdHashMapSection =
+        StorageMap<_, Blake2_128Concat, IdType, HashType, ValueQuery>;
+
+    #[pallet::storage]
+    pub(super) type IdHashMapArticle =
+        StorageMap<_, Blake2_128Concat, IdType, HashType, ValueQuery>;
+
+	#[pallet::event]
+	#[pallet::generate_deposit(pub(super) fn deposit_event)]
+	pub enum Event<T: Config> {
+		/// Event documentation should end with an array that provides descriptive names for event
+		/// parameters. [who, something]
+		SectionCreate(T::AccountId, Section),
+		SectionUpdate(T::AccountId, Section),
+		SectionDelete(T::AccountId, IdType),
+		ArticleCreate(T::AccountId, Article),
+		ArticleUpdate(T::AccountId, Article),
+		ArticleDelete(T::AccountId, IdType),
+	}
+
+	#[pallet::error]
+	pub enum Error<T> {
+		/// Error names should be descriptive.
+		NoneValue,
+		/// Errors should have helpful documentation associated with them.
+		StorageOverflow,
+	}
+
+	#[pallet::call]
+	impl<T: Config> Pallet<T> {
+		/// An example dispatchable that takes a singles value as a parameter, writes the value to
+		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
+		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
+		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
+			let who = ensure_signed(origin)?;
+
+			<Something<T>>::put(something);
+
+			Self::deposit_event(Event::SomethingStored(something, who));
+			Ok(())
+		}
+
+		/// An example dispatchable that may throw a custom error.
+		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
+		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
+			let _who = ensure_signed(origin)?;
+
+			match <Something<T>>::get() {
+				None => return Err(Error::<T>::NoneValue.into()),
+				Some(old) => {
+					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
+					<Something<T>>::put(new);
+					Ok(())
+				},
+			}
+		}
+	}
+}
